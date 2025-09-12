3e32090997f8893b6d1aa7e54ee1101a8718bb93
Marian <marian.terinte@gmail.com>
2025-09-13 00:00:52 +0300

    added more stories.

---
+++ XooCreator.BA/Migrations/20250912205556_initial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace XooCreator.BA.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Intentionally left empty.
+            // The schema was created by the previous migration '20250906130422_InitialCreate'.
+            // This migration is a no-op to keep the migration history in sync without duplicating schema creation.
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Intentionally left empty (no-op).
+        }
+    }
+}
