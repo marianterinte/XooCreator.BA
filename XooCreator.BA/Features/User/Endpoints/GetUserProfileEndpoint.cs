e6d01964e86923382693a73078334f7737cbaebd
Marian <marian.terinte@gmail.com>
2025-09-17 20:54:26 +0300

    commented out old endpoint.

--- XooCreator.BA/Features/User/Endpoints/GetUserProfileEndpoint.cs
+++ XooCreator.BA/Features/User/Endpoints/GetUserProfileEndpoint.cs
@@ -1,35 +1,39 @@
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
-using XooCreator.BA.Infrastructure.Endpoints;
-using XooCreator.BA.Infrastructure;
+//Obsolete: The Endpoint feature is deprecated. 
+    
+    
 
-namespace XooCreator.BA.Features.User.Endpoints;
+//using Microsoft.AspNetCore.Http.HttpResults;
+//using Microsoft.AspNetCore.Mvc;
+//using XooCreator.BA.Infrastructure.Endpoints;
+//using XooCreator.BA.Infrastructure;
 
-[Endpoint]
-public class GetUserProfileEndpoint
-{
-    private readonly IUserProfileService _userProfileService;
-    private readonly IUserContextService _userContext;
+//namespace XooCreator.BA.Features.User.Endpoints;
 
-    public GetUserProfileEndpoint(IUserProfileService userProfileService, IUserContextService userContext)
-    {
-        _userProfileService = userProfileService;
-        _userContext = userContext;
-    }
+//[Endpoint]
+//public class GetUserProfileEndpoint
+//{
+//    private readonly IUserProfileService _userProfileService;
+//    private readonly IUserContextService _userContext;
 
-    [Route("/api/user/profile")] // GET
-    public static async Task<Results<Ok<GetUserProfileResponse>, UnauthorizedHttpResult, NotFound>> HandleGet(
-        [FromServices] GetUserProfileEndpoint ep)
-    {
-        var userId = await ep._userContext.GetUserIdAsync();
-        if (userId == null) 
-            return TypedResults.Unauthorized();
+//    public GetUserProfileEndpoint(IUserProfileService userProfileService, IUserContextService userContext)
+//    {
+//        _userProfileService = userProfileService;
+//        _userContext = userContext;
+//    }
 
-        var result = await ep._userProfileService.GetUserProfileAsync(userId.Value);
+//    [Route("/api/user/profile")] // GET
+//    public static async Task<Results<Ok<GetUserProfileResponse>, UnauthorizedHttpResult, NotFound>> HandleGet(
+//        [FromServices] GetUserProfileEndpoint ep)
+//    {
+//        var userId = await ep._userContext.GetUserIdAsync();
+//        if (userId == null) 
+//            return TypedResults.Unauthorized();
+
+//        var result = await ep._userProfileService.GetUserProfileAsync(userId.Value);
         
-        if (!result.Success || result.User == null)
-            return TypedResults.NotFound();
+//        if (!result.Success || result.User == null)
+//            return TypedResults.NotFound();
 
-        return TypedResults.Ok(result);
-    }
-}
+//        return TypedResults.Ok(result);
+//    }
+//}
