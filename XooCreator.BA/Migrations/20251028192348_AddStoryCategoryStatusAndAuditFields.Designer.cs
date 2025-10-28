a7783c0fffe6aae3e417df2e86362261cfa8ce61
marianterinte <marian.terinte@gmail.com>
2025-10-28 21:23:58 +0200

    DEV-MarketPlace

--- XooCreator.BA/Migrations/20251028192348_AddStoryCategoryStatusAndAuditFields.Designer.cs
+++ XooCreator.BA/Migrations/20251028192348_AddStoryCategoryStatusAndAuditFields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using XooCreator.BA.Data;
@@ -11,9 +12,11 @@ using XooCreator.BA.Data;
 namespace XooCreator.BA.Migrations
 {
     [DbContext(typeof(XooDbContext))]
-    partial class XooDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251028192348_AddStoryCategoryStatusAndAuditFields")]
+    partial class AddStoryCategoryStatusAndAuditFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
